9fa1f2f09e5750ba28a17127da62bc389d528766
SierraDavis2000 <99284604+SierraDavis2000@users.noreply.github.com>
2022-03-19 20:26:48 -0400

    Quiz questions

---
+++ script.js
@@ -0,0 +1,42 @@
+let question = [
+    {
+        question: "Which of the following is not a programing language?",
+        choiceA: "Python",
+        choiceB: "Java",
+        choiceC: "TypeScript",
+        choiceD: "Rattlesnake",
+        correct: "D"
+    },
+    {
+        question: "What does HTML stand for?",
+        choiceA: "HyperText Markup Language",
+        choiceB: "Home Tool Markup Language",
+        choiceC: "High Traffic Media Language",
+        choiceD: "High Text Markup Language",
+        correct: "A"
+    },
+    {
+        question: "Where is the JavaScript placed inside an HTML document or page?",
+        choiceA: "In the <body> and <head> sections",
+        choiceB: "In the <footer> section",
+        choiceC: "In the <meta> section",
+        choiceD: "In the <title> section",
+        correct: "A"
+    },
+    {
+        question: "In JavaScript, what element is used to store and manipulate text, usually in multiples?",
+        choiceA: "Arrays",
+        choiceB: "Strings",
+        choiceC: "Variables",
+        choiceD: "Recorders",
+        correct: "B"
+    },
+    {
+        question: "Which is the largest unit of storage?",
+        choiceA: "Megabyte",
+        choiceB: "Gigabyte",
+        choiceC: "Terabyte",
+        choiceD: "Kilobyte",
+        correct: "C"
+    },
+]
