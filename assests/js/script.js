a5afeb1845b9aea02bf16d8f6b0687d1a761db34
SierraDavis2000 <99284604+SierraDavis2000@users.noreply.github.com>
2022-07-17 22:14:00 -0400

    new attempt code

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,115 +1,231 @@
-const question = document.getElementById("question");
-const choices = Array.from(document.getElementById("choices"))
-const scoreText = document.getElementById("score")
+var start = document.querySelector("#startpage");
+var startBtn = document.querySelector("#start_button");
+var introPage =document.querySelector("#intro_page");
 
-let currentQuestion = {}
-let acceptingAnswers = true
-let score = 0
-let questionCounter = 0
-let availableQuestions = []
+var quizPage = document.querySelector("#quiz_page");
+var currentQuestion = document.querySelector("#current_question");
+
+var reactButtons = document.querySelectorAll(".choices");
+var answerBtn1 = document.querySelector("#answer_btn1");
+var answerBtn2 = document.querySelector("#answer_btn2");
+var answerBtn3 = document.querySelector("#answer_btn3");
+var answerBtn4 = document.querySelector("#answer_btn4");
+
+var checkLine = document.querySelector("#check_line");
+var scoreBoard = document.querySelector("#submit_page");
+var finalScore = document.querySelector("#final_score");
+var userInitial =document.querySelector("#initial");
+
+var submitBtn =document.querySelector("#submit_btn");
+var highScorePage =document.querySelector("#highscore_page");
+var scoreRecord =document.querySelector("#score_record");
+var scoreCheck =document.querySelector("#score_check");
+var finish =document.querySelector("#finish");
+
+var backBtn =document.querySelector("#back_btn");
+var clearBtn=document.querySelector("#clear_btn");
+
+
+//
 
 
 let questions = [
     {
         question: "Which of the following is not a programing language?",
-        choice1:"Python",
-        choice2: "Java",
-        choice3: "TypeScript",
-        choice4: "Rattlesnake",
-        answer: 4,
+        choices:["A. Python", "B. Java", "C. TypeScript", "D. Rattlesnake"],
+        answer: "D",
 
     },
     {
         question: "What does HTML stand for?",
-        choice1:"HyperText Markup Language",
-        choice2: "Home Tool Markup Language",
-        choice3: "High Traffic Media Language",
-        choice4: "High Text Markup Language",
-        answer: 1,
+        choices:["A. HyperText Markup Language", "B. Home Tool Markup Language", "C. High Traffic Media Language", "D. High Text Markup Language"],
+        answer: "A",
     },
     {
         question: "Where is the JavaScript placed inside an HTML document or page?",
-        choice1:"In the <body> and <head> sections",
-        choice2: "In the <footer> section",
-        choice3: "In the <meta> section",
-        choice4: "In the <title> section",
-        answer: 1,
+        choices:["A. In the <body> and <head> sections", "B. In the <footer> section", "C. In the <meta> section", "D. In the <title> section"], 
+        answer: "A",
       
     },
     {
         question: "In JavaScript, what element is used to store and manipulate text, usually in multiples?",
-        choice1:"Arrays",
-        choice2: "Strings",
-        choice3: "Variables",
-        choice4: "Recorders",
-        answer: 2,
+        choices:["A. Arrays", "B. Strings", "C. Variables", "D. Recorders"],
+        answer: "B",
     
     },
     {
         question: "Which is the largest unit of storage?",
-        choice1:"Megabyte",
-        choice2: "Terabyte",
-        choice3: "Gigabyte",
-        choice4: "Kilobyte",
-        answer: 2,
+        choices:["A. Megabyte", "B. Terabyte", "C. Gigabyte", "D. Kilobyte"],
+        answer: "B",
     }
 ]
 
-const SCORE_POINTS = 100
-const MAX_QUESTIONS = 5
+var timeRemaining = document.getElementById("timer");
 
-function startQuiz(){
-    questionCounter = 0
-    score = 0 
-    availableQuestions = [...questions]
-    getNewQuestion()
+var secondsLeft = 60;
+var questionNumber = 0;
+var totalScore = 0;
+var questionCount = 1;
+
+function timer() {
+        
+    var timerInterval = setInterval(function () {
+
+      secondsLeft--;
+      timeRemaining.textContent = "Time left: " + secondsLeft + " s";
+
+        if (secondsLeft <= 0){
+            clearInterval(timerInterval);
+            timeLeft.textContent = "The Quiz is Now Over"; 
+            finish.textContent = "The Quiz is Now Over";
+            finishQuiz();
+
+        } else  if(questionCount >= questions.length +1) {
+            clearInterval(timerInterval);
+            finishQuiz();
+            } 
+}, 1000);
 }
 
-function getNewQuestion(){
-    if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS){
-        localStorage.setItem('mostRecentScore', score)
+function startQuiz () {
+    introPage.style.display = "none";
+    quizPage.style.display = "block";
+    questionNumber = 0
+    timer();
+    showQuestion(questionNumber);
+  
+}
 
-        return window.location.assign('/end.html')
+function showQuestion (n) {
+    currentQuestion.textContent = questions[n].question;
+    answerBtn1.textContent = questions[n].choices[0];
+    answerBtn2.textContent = questions[n].choices[1];
+    answerBtn3.textContent = questions[n].choices[2];
+    answerBtn4.textContent = questions[n].choices[3];
+    questionNumber = n;
+}
+
+function checkAnswer(event) {
+    event.preventDefault();
+    checkLine.style.display = "block";
+    setTimeout(function () {
+        checkLine.style.display = 'none';
+    }, 1000);
+
+    if (questions[questionNumber].answer == event.target.value) {
+        checkLine.textContent = "Correct!"; 
+        totalScore = totalScore + 1;
+
+    } else {
+        secondsLeft = secondsLeft - 10;
+        checkLine.textContent = "Incorrect! The correct answer is " + questions[questionNumber].answer + " .";
     }
-    questionCounter++
-
-    const questionIndex = Math.floor(Math.random()*availableQuestions.length)
-    currentQuestion = availableQuestions[questionIndex]
-    question.innerText = currentQuestion.question
-    choices.forEach(choice => {
-        const number = choice.dataset['number']
-        choice.innerText = currentQuestion[choice + number]
-    })
-
-    availableQuestions.splice(questionIndex, 1)
-    acceptingAnswers = true
+    if (questionNumber < questionSource.length -1 ) {
+        showQuestion(questionNumber +1);
+    } else {
+    finishQuiz();
+    }
+    questionCount++;
 }
 
-incrementScore = num => {
-    score += num
-    scoreText.innerText = score
+function finishQuiz() {
+
+    quizPage.style.display = "none";
+    scoreBoard.style.display = "block";
+    console.log(scoreBoard);
+    finalScore.textContent = "Your final score is :" + totalScore ; 
+    timeLeft.style.display = "none"; 
+};
+
+function getScore () {
+var currentList =localStorage.getItem("ScoreList");
+if (currentList !== null ){
+    freshList = JSON.parse(currentList);
+    return freshList;
+} else {
+    freshList = [];
+}
+return freshList;
+};
+
+
+// render score to the score board
+function renderScore () {
+    scoreRecord.innerHTML = "";
+    scoreRecord.style.display ="block";
+    var highScores = sort();   
+};
+
+
+function sort () {
+    var unsortedList = getScore();
+    if (getScore == null ){
+        return;
+    } else{
+    unsortedList.sort(function(a,b){
+        return b.score - a.score;
+    })
+    return unsortedList;
+}};
+
+
+function addItem (n) {
+    var addedList = getScore();
+    addedList.push(n);
+    localStorage.setItem("ScoreList", JSON.stringify(addedList));
+};
+
+function saveScore () {
+    var scoreItem ={
+        user: userInitial.value,
+        score: totalScore
+    }
+    addItem(scoreItem);
+    renderScore();
 }
 
-startQuiz()
 
-choices.forEach(choice => {
-    choice.addEventListener('click', e => {
-        if(!acceptingAnswers) return
+startBtn.addEventListener("click", startQuiz);
 
-        acceptingAnswers = false
-        const selectedChoice = e.target
-        const selectedAnswer = selectedChoice.dataset['number']
 
-        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+reactButtons.forEach(function(click){
 
-        if (classToApply === 'correct'){
-            incrementScore(SCORE_POINTS)
-        } 
-        selectedChoice.parentElement.classList.add(classToApply)
+    click.addEventListener("click", checkAnswer);
+});
 
-        setTimeout(() => {
-            selectedChoice.parentElement.classList.remove(classToApply)
-            getNewQuestion()
-        }, 1000)
-    })
-})
+
+submitBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    scoreBoard.style.display = "none";
+    introPage.style.display = "none";
+    highScorePage.style.display = "block";
+    questionPage.style.display ="none";
+    saveScore();
+});
+
+
+scoreCheck.addEventListener("click", function(event) {
+    event.preventDefault();
+    scoreBoard.style.display = "none";
+    introPage.style.display = "none";
+    highScorePage.style.display = "block";
+    questionPage.style.display ="none";
+    renderScore();
+});
+
+//go back to main page
+backBtn.addEventListener("click",function(event){
+        event.preventDefault();
+        scoreBoard.style.display = "none";
+        introPage.style.display = "block";
+        highScorePage.style.display = "none";
+        questionPage.style.display ="none";
+        location.reload();
+});
+
+//clear local storage and clear page shows
+clearBtn.addEventListener("click",function(event) {
+    event.preventDefault();
+    localStorage.clear();
+    renderScore();
+});
